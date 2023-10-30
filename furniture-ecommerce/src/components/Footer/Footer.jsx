af2dd6b523ae10a0b01304be88e2b9a83b4d1b00
anandadr5 <anandadwir898@gmail.com>
2023-10-30 20:30:16 +0700

    add footer

---
+++ furniture-ecommerce/src/components/Footer/Footer.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+import "../../styles/footer.css";
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="4" className="mb-4" md="6">
+            <div className="logo">
+              <div>
+                <h1>RuangImpian</h1>
+              </div>
+            </div>
+            <p className="footer__text mt-4">
+              RuangImpian adalah toko online yang menyediakan berbagai jenis
+              furnitur dan peralatan rumah tangga. Anda dapat menemukan berbagai
+              produk berkualitas untuk menghias dan mempercantik rumah Anda.
+            </p>
+          </Col>
+          <Col lg="3" md="3" className="mb-4">
+            <div className="footer__quick_links">
+              <h4 className="quick__links-title">Top Categories</h4>
+              <ListGroup>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Arm Chair</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Modern Sofa</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Bed Frame</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Chests</Link>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+          <Col lg="2" md="3" className="mb-4">
+            <div className="footer__quick_links">
+              <h4 className="quick__links-title">Useful Links</h4>
+              <ListGroup>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="/shop">Shop</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="/cart">Cart</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="/login">Login</Link>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+          <Col lg="3" md="4">
+            <div className="footer__quick_links">
+              <h4 className="quick__links-title">Contact</h4>
+              <ListGroup className="footer__contact">
+                <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i className="ri-map-pin-line"></i>
+                  </span>
+                  <p>Tangerang, Banten</p>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i className="ri-phone-line"></i>
+                  </span>
+                  <p>+6281773929</p>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i className="ri-mail-line"></i>
+                  </span>
+                  <p>ruangimpian@gmail.com</p>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+
+          <Col lg="12">
+            <p className="footer__copyright">
+              Created by Ananda Dwi Rizkyta | © {year}
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
