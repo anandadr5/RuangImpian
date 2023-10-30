cc533f8f0cfdb2bf6268c58c2710d8d869b870cd
anandadr5 <anandadwir898@gmail.com>
2023-10-30 20:35:12 +0700

    add routes

---
+++ furniture-ecommerce/src/routes/Routers.jsx
@@ -0,0 +1,34 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import Home from "../pages/Home";
+import Shop from "../pages/Shop";
+import Cart from "../pages/Cart";
+import ProductDetails from "../pages/ProductDetails";
+import History from "../pages/History";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import ProtectedRoute from "./ProtectedRoute";
+
+const Routers = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="home" />} />
+      <Route path="home" element={<Home />} />
+      <Route path="shop" element={<Shop />} />
+      <Route path="shop/:id" element={<ProductDetails />} />
+      <Route path="cart" element={<Cart />} />
+      <Route
+        path="history"
+        element={
+          <ProtectedRoute>
+            <History />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="login" element={<Login />} />
+      <Route path="signup" element={<Signup />} />
+    </Routes>
+  );
+};
+
+export default Routers;
