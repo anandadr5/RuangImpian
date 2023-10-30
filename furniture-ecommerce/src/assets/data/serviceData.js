07a939d94120a469ce7ff7ffdd9ad77253551e52
anandadr5 <anandadwir898@gmail.com>
2023-10-30 20:35:52 +0700

    add serviceData assets

---
+++ furniture-ecommerce/src/assets/data/serviceData.js
@@ -0,0 +1,28 @@
+const serviceData = [
+  {
+    icon: "ri-truck-line",
+    title: "Free Shipping",
+    subtitle: "Best Free Shipping Offer For You",
+    bg: "#fdefe6",
+  },
+  {
+    icon: "ri-refresh-line",
+    title: "Easy Returns",
+    subtitle: "Hassle-Free Returns Made Simple.",
+    bg: "#ceebe9",
+  },
+  {
+    icon: "ri-secure-payment-line",
+    title: "Secure Payment",
+    subtitle: "Your Transactions, Your Peace of Mind.",
+    bg: "#e2f2b2",
+  },
+  {
+    icon: "ri-exchange-dollar-line",
+    title: " Back Guarantee",
+    subtitle: "Your Satisfaction, Our Promise",
+    bg: "#d6e5fb",
+  },
+];
+
+export default serviceData;
