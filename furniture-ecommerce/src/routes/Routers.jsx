2cbd4b4476f8ae0c5a765842ea477f2148ef0c7f
anandadr5 <anandadwir898@gmail.com>
2023-10-31 23:42:23 +0700

    add navigate chat

--- furniture-ecommerce/src/routes/Routers.jsx
+++ furniture-ecommerce/src/routes/Routers.jsx
@@ -7,6 +7,7 @@ import ProductDetails from "../pages/ProductDetails";
 import History from "../pages/History";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
+import Chat from "../components/OpenAI/Chat";
 import ProtectedRoute from "./ProtectedRoute";
 
 const Routers = () => {
@@ -15,6 +16,7 @@ const Routers = () => {
       <Route path="/" element={<Navigate to="home" />} />
       <Route path="home" element={<Home />} />
       <Route path="shop" element={<Shop />} />
+      <Route path="chat" element={<Chat />} />
       <Route path="shop/:id" element={<ProductDetails />} />
       <Route path="cart" element={<Cart />} />
       <Route
