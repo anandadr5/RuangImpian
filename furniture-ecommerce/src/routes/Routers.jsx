054001585e856d93939d83f6dbcdd3a2d050445a
anandadr5 <anandadwir898@gmail.com>
2023-11-01 01:12:18 +0700

    update routes

--- furniture-ecommerce/src/routes/Routers.jsx
+++ furniture-ecommerce/src/routes/Routers.jsx
@@ -7,6 +7,7 @@ import ProductDetails from "../pages/ProductDetails";
 import History from "../pages/History";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
+import Profile from "../pages/Profile";
 import Chat from "../components/OpenAI/Chat";
 import ProtectedRoute from "./ProtectedRoute";
 
@@ -17,6 +18,7 @@ const Routers = () => {
       <Route path="home" element={<Home />} />
       <Route path="shop" element={<Shop />} />
       <Route path="chat" element={<Chat />} />
+      <Route path="profile" element={<Profile />} />
       <Route path="shop/:id" element={<ProductDetails />} />
       <Route path="cart" element={<Cart />} />
       <Route
