43150c50c0db974a17c13dfdbb561fc9da8639c2
anandadr5 <anandadwir898@gmail.com>
2023-11-03 20:44:55 +0700

    update

--- furniture-ecommerce/src/components/OpenAI/Chat.jsx
+++ furniture-ecommerce/src/components/OpenAI/Chat.jsx
@@ -1,57 +1,79 @@
-import React, { useState } from "react";
-import OpenAI from "openai";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "../../styles/chat.css";
 
-const openai = new OpenAI({
-  apiKey: import.meta.env.VITE_OPENAI_API_KEY,
-  dangerouslyAllowBrowser: true,
-});
+const Chat = () => {
+  const [inputText, setInputText] = useState("");
+  const [chatHistory, setChatHistory] = useState([]);
+  const apiKey = import.meta.env.VITE_OPENAI_API_KEY;
 
-export default function Index() {
-  const [prompt, setPrompt] = useState("");
-  const [result, setResult] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    if (chatHistory.length === 0) return;
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+    const latestMessage = chatHistory[chatHistory.length - 1];
+    if (latestMessage.role === "user") {
+      sendAIResponse();
+    }
+  }, [chatHistory]);
 
+  const sendAIResponse = async () => {
     try {
-      setIsLoading(true);
+      const userMessages = chatHistory
+        .filter((message) => message.role === "user")
+        .map((message) => message.content)
+        .join("\n");
 
-      const response = await openai.completions.create({
-        model: "davinci",
-        prompt: `User: Rekomendasikan warna sofa yang cocok untuk ruang tamu minimalis dengan gaya dekorasi modern. ${prompt}`,
-        max_tokens: 150,
-      });
+      const prompt = `Ini adalah website e-commerce furnitur. ${userMessages}`;
 
-      setResult(response.choices[0].text);
+      const response = await axios.post(
+        "https://api.openai.com/v1/engines/davinci/completions",
+        {
+          prompt: prompt,
+          max_tokens: 50,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${apiKey}`,
+          },
+        }
+      );
+
+      const aiMessage = { role: "ai", content: response.data.choices[0].text };
+      setChatHistory([...chatHistory, aiMessage]);
     } catch (error) {
-      console.log(error);
-    } finally {
-      setIsLoading(false);
+      console.error("Error sending message:", error);
     }
   };
 
+  const sendMessage = () => {
+    if (inputText.trim() === "") return;
+
+    const message = { role: "user", content: inputText };
+    setChatHistory([...chatHistory, message]);
+    setInputText("");
+  };
+
   return (
-    <div className="container">
-      <form onSubmit={handleSubmit}>
-        <label>
-          Pertanyaan:
-          <input
-            type="text"
-            value={prompt}
-            onChange={(e) => setPrompt(e.target.value)}
-          />
-        </label>
-        <button type="submit">Kirim</button>
-      </form>
-      {isLoading && <p className="loading">Loading...</p>}
-      {result && (
-        <div>
-          <h2>Jawaban:</h2>
-          <p className="response">{result}</p>
-        </div>
-      )}
+    <div className="chat-container">
+      <div className="chat-history">
+        {" "}
+        {chatHistory.map((message, index) => (
+          <div key={index} className={`message ${message.role}`}>
+            {message.content}
+          </div>
+        ))}
+      </div>
+      <div className="chat-input">
+        <input
+          type="text"
+          placeholder="Type your message..."
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+        />
+        <button onClick={sendMessage}>Send</button>
+      </div>
     </div>
   );
-}
+};
+
+export default Chat;
